20c3b251fafda6f34dfbefb7a9494560d42dff79
Felixwang91 <felix.wang91@gmail.com>
2014-02-27 11:07:10 -0600

    Find the file length and predefine the array rows

--- src/cse487/Scheduler/model/scheduling/ReadWithScanner.java
+++ src/cse487/Scheduler/model/scheduling/ReadWithScanner.java
@@ -6,15 +6,26 @@ import java.util.*;
 public class ReadWithScanner {
 	File file;
 	String parsed[][];
+	String filename;
 
 	public ReadWithScanner(String filename) {
 		File file = new File(filename);
+		this.filename = filename;
+	}
+	
+	public int FileLengthCounter() throws IOException{//Counter for counting the file length
+		BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+		int lines = 0;
+		while (reader.readLine() != null) lines++;
+		reader.close();
+		return lines;
 	}
 
 	public static void main(String args[]) throws IOException {
 		ReadWithScanner reader = new ReadWithScanner("/pathOfFile/Filename.text");
 		Scanner input = new Scanner(reader.file);
-		String field[][] = new String[200][35];
+		int fileLines = reader.FileLengthCounter();
+		String field[][] = new String[fileLines][35];
 		int row = 0, colm = 0;
 
 		//Reading the input file and put the data into class variable parsed[][].
@@ -36,5 +47,7 @@ public class ReadWithScanner {
 		input.close();
 
 		reader.parsed = field;
+		
 	}
+
 }
