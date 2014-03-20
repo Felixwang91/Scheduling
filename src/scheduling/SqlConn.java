d931d1a2d7208c63d325ca154d3c8cb32f108d31
Felixwang91 <felix.wang91@gmail.com>
2014-03-19 22:50:25 -0500

    Update on Database

--- src/scheduling/SqlConn.java
+++ src/scheduling/SqlConn.java
@@ -6,9 +6,8 @@ import java.sql.DriverManager;
 public class SqlConn {
     public static void main(String[] arg) {
         try {
-            Connection con = null;
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/scheduling", "root", "");
+            DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/scheduling", "root", "");
             System.out.print("yes");
         } catch (Exception e) {
             System.out.print("MYSQL ERROR:" + e.getMessage());
